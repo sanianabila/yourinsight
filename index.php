b7367eede455fb2190ebc8957b527caa34fb2e4c
sanianabila <113865227+sanianabila@users.noreply.github.com>
2024-06-17 00:12:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -101,13 +101,13 @@ if (!empty($search_keyword)) {
     <!-- Responsive navbar-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="http://localhost/belajar web">Your Insight</a>
+            <a class="navbar-brand" href="http://yourinsight.infinityfreeapp.com/">Your Insight</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="http://localhost/belajar web">Beranda</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#tentang">Tentang</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#kontak">Kontak</a></li>
+                    <li class="nav-item"><a class="nav-link" href="http://yourinsight.infinityfreeapp.com/">Beranda</a></li>
+                    <li class="nav-item"><a class="nav-link" href="http://yourinsight.infinityfreeapp.com/#tentang">Tentang</a></li>
+                    <li class="nav-item"><a class="nav-link" href="http://yourinsight.infinityfreeapp.com/#kontak">Kontak</a></li>
                 </ul>
             </div>
         </div>
